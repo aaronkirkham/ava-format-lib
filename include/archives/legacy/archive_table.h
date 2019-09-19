65ed67784b1e1102792a23b36af581833ff26dd7
aaronkirkham07@gmail.com <aaronkirkham07@gmail.com>
2019-09-19 19:17:16 +0100

    rename legacy TAB structs

--- include/archives/legacy/archive_table.h
+++ include/archives/legacy/archive_table.h
@@ -8,22 +8,22 @@ namespace ava::legacy::ArchiveTable
 static constexpr uint32_t TAB_MAGIC = 0x424154; // "TAB"
 
 #pragma pack(push, 1)
-struct TabFileHeader {
+struct TabHeader {
     uint32_t m_Magic     = TAB_MAGIC;
     uint16_t m_Version   = 2;
     uint16_t m_Endian    = 1;
     int32_t  m_Alignment = 0x1000;
 };
 
-struct TabFileEntry {
+struct TabEntry {
     uint32_t m_NameHash;
     uint32_t m_Offset;
     uint32_t m_Size;
 };
 #pragma pack(pop)
 
-static_assert(sizeof(TabFileHeader) == 0xC, "TabFileHeader (legacy) alignment is wrong!");
-static_assert(sizeof(TabFileEntry) == 0xC, "TabFileEntry (legacy) alignment is wrong!");
+static_assert(sizeof(TabHeader) == 0xC, "TabHeader (legacy) alignment is wrong!");
+static_assert(sizeof(TabEntry) == 0xC, "TabEntry (legacy) alignment is wrong!");
 
-void ReadTab(const std::vector<uint8_t>& buffer, std::vector<TabFileEntry>* out_entries);
+void ReadTab(const std::vector<uint8_t>& buffer, std::vector<TabEntry>* out_entries);
 }; // namespace ava::legacy::ArchiveTable
--- src/archives/legacy/archive_table.cpp
+++ src/archives/legacy/archive_table.cpp
@@ -5,7 +5,7 @@
 
 namespace ava::legacy::ArchiveTable
 {
-void ReadTab(const std::vector<uint8_t>& buffer, std::vector<TabFileEntry>* out_entries)
+void ReadTab(const std::vector<uint8_t>& buffer, std::vector<TabEntry>* out_entries)
 {
     if (buffer.empty()) {
         throw std::invalid_argument("TAB input buffer can't be empty!");
@@ -19,16 +19,16 @@ void ReadTab(const std::vector<uint8_t>& buffer, std::vector<TabFileEntry>* out_
     std::istream      stream(&buf);
 
     // read header
-    TabFileHeader header;
-    stream.read((char*)&header, sizeof(TabFileHeader));
+    TabHeader header;
+    stream.read((char*)&header, sizeof(TabHeader));
     if (header.m_Magic != TAB_MAGIC) {
         throw std::runtime_error("Invalid header magic! (Input file isn't .TAB?)");
     }
 
     // read entries
     while (!stream.eof()) {
-        TabFileEntry entry;
-        stream.read((char*)&entry, sizeof(TabFileEntry));
+        TabEntry entry;
+        stream.read((char*)&entry, sizeof(TabEntry));
 
         if (stream.fail()) {
             break;
