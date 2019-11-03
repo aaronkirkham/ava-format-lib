0431554fe972b0d011bf2b3db89d48b5eeb29fec
aaronkirkham07@gmail.com <aaronkirkham07@gmail.com>
2019-11-03 00:40:25 +0000

    update configure script

--- configure.sh
+++ configure.sh
@@ -1,25 +1,12 @@
 #/bin/bash
 
-case "$OSTYPE" in
-  linux*)             BINARY="./tools/premake5";      ACTION="gmake2";   ARCHIVE="linux.tar.gz" ;;
-  darwin*)            BINARY="./tools/premake5";      ACTION="xcode4";   ARCHIVE="macosx.tar.gz" ;;
-  win|msys|cygwin*)   BINARY="./tools/premake5.exe";  ACTION="vs2017";   ARCHIVE="windows.zip" ;;
-  *)                  echo "Error! Unknown OS: $OSTYPE"; exit ;;
-esac
-
-if [ ! -f "$BINARY" ]; then
+if [ ! -f "./premake5.exe" ]; then
   echo "Downloading build tools..."
 
-  mkdir -p "./tools"
-  curl -L -sS "https://github.com/premake/premake-core/releases/download/v5.0.0-alpha14/premake-5.0.0-alpha14-$ARCHIVE" -o "./tools/$ARCHIVE"
+  curl -L -sS "https://github.com/premake/premake-core/releases/download/v5.0.0-alpha14/premake-5.0.0-alpha14-windows.zip" -o "./premake.zip"
 
-  if [[ $ARCHIVE == *".zip" ]]; then
-    unzip -q "./tools/$ARCHIVE" -d tools
-  else
-    tar -zxvf "./tools/$ARCHIVE" -C tools
-  fi
-
-  rm "./tools/$ARCHIVE"
+  unzip -q "./premake.zip"
+  rm "./premake.zip"
 fi
 
-$BINARY --file=premake.lua $ACTION
+./premake5.exe --file=premake.lua vs2017
